c126b5bc32e494d0a5755bc8c38e92f7c42d7ac1
David Feltell <dave@feltell.net>
2022-02-20 02:00:27 +0000

    Add Python `ctypes`-based binding demo

---
+++ tests/demos/string_map/app/main.py
@@ -0,0 +1,61 @@
+import ctypes
+import os
+from ctypes import CFUNCTYPE, c_char_p, c_int, c_void_p, c_char
+
+lib_path = os.getenv("FELTPLUGINSYSTEM_DEMO_HOST_LIB")
+assert lib_path is not None, "FELTPLUGINSYSTEM_DEMO_HOST_LIB must be set to host library path"
+
+host = None
+
+
+class fpdemo_String_s(ctypes.Structure):
+    _fields_ = [
+        ("create", CFUNCTYPE(c_int, c_char_p, c_void_p)),
+        ("release", CFUNCTYPE(None, c_void_p)),
+        ("assign_cstr", CFUNCTYPE(c_int, c_char_p, c_void_p, c_char_p)),
+        ("assign_StringView", CFUNCTYPE(c_int, c_char_p, c_void_p, c_void_p)),
+        ("c_str", CFUNCTYPE(c_char_p, c_void_p)),
+        ("at", CFUNCTYPE(c_int, c_char_p, c_char_p, c_void_p, c_int)),
+    ]
+
+
+class CString:
+    def __init__(self, s: str):
+        self.__csuite = host.fpdemo_String_suite()
+        self.__cerr = ctypes.create_string_buffer(500)
+        self.__chandle = c_void_p()
+
+        code = self.__csuite.create(self.__cerr, ctypes.byref(self.__chandle))
+        if code != 0:
+            raise RuntimeError("Error code '%s' from C with message: '%s'" % (code, self.__cerr))
+
+        self.__csuite.assign_cstr(self.__cerr, self.__chandle, s.encode())
+
+    def __del__(self):
+        self.__csuite.release(self.__chandle)
+
+    def c_str(self):
+        return self.__csuite.c_str(self.__chandle)
+
+    def at(self, pos: int):
+        out = c_char()
+        code = self.__csuite.at(self.__cerr, ctypes.byref(out), self.__chandle, pos)
+        if code != 0:
+            raise RuntimeError(
+                "Error code '%s' from C with message: '%s'" % (code, self.__cerr.value))
+        return out.value
+
+
+if __name__ == "__main__":
+    host = ctypes.CDLL(lib_path)
+    host.fpdemo_String_suite.restype = fpdemo_String_s
+
+    cstr = CString("some data")
+
+    print(cstr.c_str())
+
+    try:
+        for n in range(100):
+            print(cstr.at(n))
+    except RuntimeError as ex:
+        print(ex)
