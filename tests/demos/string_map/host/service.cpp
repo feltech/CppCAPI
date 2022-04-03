200da2db085c05a94e14d1c2a6056bab48b30878
David Feltell <dave@feltell.net>
2022-04-03 20:36:04 +0100

    Factory -> Decorator

--- tests/demos/string_map/host/service.cpp
+++ tests/demos/string_map/host/service.cpp
@@ -45,7 +45,7 @@ extern "C"
 	using feltpluginsystemdemohost::service::String;
 	FELTPLUGINSYSTEM_DEMO_HOST_EXPORT fpdemo_String_s fpdemo_String_suite()
 	{
-		using Factory = HandleWrapper::Decorator<fpdemo_String_h>;
+		using Decorator = HandleWrapper::Decorator<fpdemo_String_h>;
 		using Converter = HandleWrapper::Converter<fpdemo_String_h>;
 
 		return {
@@ -55,27 +55,27 @@ extern "C"
 
 			.assign_cstr =
 				[](fp_ErrorMessage err, fpdemo_String_h hself, char const * cstr) {
-				return Factory::mem_fn(
+				return Decorator::mem_fn(
 					[](String & self, char const * str) { self = str; }, err, hself, cstr);
 			},
 
 			.assign_StringView =
 				[](fp_ErrorMessage err, fpdemo_String_h hself, fpdemo_StringView_h hstr)
 			{
-				return Factory::mem_fn(
+				return Decorator::mem_fn(
 					[](String & self, StringView const & str) { self = str; }, err, hself, hstr);
 			},
 
-//			.c_str = [](fpdemo_String_h handle)
-//			{ return Factory::mem_fn([](String const & self) { return self.c_str(); }, handle); },
+			//			.c_str = [](fpdemo_String_h handle)
+			//			{ return Factory::mem_fn([](String const & self) { return self.c_str(); },
+			// handle); },
 
-			.c_str = Factory::decorate(Factory::mem_fn_ptr<&String::c_str>),
-//			.at = Factory::decorate(
-//					[](String const & self, int n) { return self.at(n); })
+			.c_str = Decorator::decorate(Decorator::mem_fn_ptr<&String::c_str>),
+			//			.at = Factory::decorate(
+			//					[](String const & self, int n) { return self.at(n); })
 
-			.at = Factory::decorate([](String const & self, size_t n) { return self.at(n); })
-			};
-//		return suite;
+			.at = Decorator::decorate([](String const & self, size_t n) { return self.at(n); })};
+		//		return suite;
 	}
 
 	// StringView
@@ -84,26 +84,26 @@ extern "C"
 
 	FELTPLUGINSYSTEM_DEMO_HOST_EXPORT fpdemo_StringView_s fpdemo_StringView_suite()
 	{
-		using Factory = HandleWrapper::Decorator<fpdemo_StringView_h>;
+		using Decorator = HandleWrapper::Decorator<fpdemo_StringView_h>;
 		return {
 			.data =
-				[](fpdemo_StringView_h handle)
-			{
-				return Factory::mem_fn([](StringView const & self) { return self.data(); }, handle);
-			},
+				[](fpdemo_StringView_h handle) {
+					return Decorator::mem_fn(
+						[](StringView const & self) { return self.data(); }, handle);
+				},
 
 			.size =
-				[](fpdemo_StringView_h handle)
-			{
-				return Factory::mem_fn([](StringView const & self) { return self.size(); }, handle);
-			}};
+				[](fpdemo_StringView_h handle) {
+					return Decorator::mem_fn(
+						[](StringView const & self) { return self.size(); }, handle);
+				}};
 	}
 
 	// StringDict
 
 	FELTPLUGINSYSTEM_DEMO_HOST_EXPORT fp_StringDict_s fpdemo_StringDict_suite()
 	{
-		using Factory = HandleWrapper::Decorator<fpdemo_StringDict_h>;
+		using Decorator = HandleWrapper::Decorator<fpdemo_StringDict_h>;
 		using Converter = HandleWrapper::Converter<fpdemo_StringDict_h>;
 
 		return {
@@ -117,7 +117,7 @@ extern "C"
 				   fpdemo_String_h key,
 				   fpdemo_String_h value)
 			{
-				return Factory::mem_fn(
+				return Decorator::mem_fn(
 					[](StringDict & self, String key, String value)
 					{ return self.insert_or_assign(std::move(key), std::move(value)); },
 					err,
@@ -132,7 +132,7 @@ extern "C"
 				   fpdemo_StringDict_h handle,
 				   fpdemo_String_h key)
 			{
-				return Factory::mem_fn(
+				return Decorator::mem_fn(
 					[](StringDict const & self, String const & key) { return self.at(key); },
 					err,
 					out,
